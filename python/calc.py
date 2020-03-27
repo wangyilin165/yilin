ee7fcd93cec7800cccc1e385fe2815423d894631
wangyilin165 <444852198@qq.com>
2020-03-27 10:31:57 +0800

    测试calc的加法、除法功能

--- python/calc.py
+++ python/calc.py
@@ -1,6 +1,6 @@
 class Calc:
-    def div(self, a, b):
+    def div(self, a, b) -> float:
         return a / b
 
-    def add(self, a, b):
+    def add(self, a: int, b: int) -> int:
         return a + b
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,33 +1,62 @@
 import pytest
-
 from python.calc import Calc
+
+'''
+测试加法：
+假设，a,b输入为int类型
+测试：a,b为正整数 
+     a,b为负整数
+     a,b为正小数
+     a,b为负小数
+     a,b为正虚数
+     a,b为负虚数
+     a,b为二进制数
+     
+测试除法：
+假设：a,b为float类型
+测试：a=0,b为非零数
+     a=非零数，b=0
+     a,b为正整数 
+     a,b为负整数
+     a,b为正小数
+     a,b为负小数
+     a,b为正虚数
+     a,b为负虚数
+'''
+test_add_date = [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (0.1, 0.2, 0.3),
+    (-0.1, -0.2, -0.3),
+    (complex(1, 1), complex(1, 1), complex(2, 2)),
+    (complex(-1, -1), complex(-1, -1), complex(-2, -2)),
+    (bin(1), bin(1), bin(2))
+]
+
+test_div_date = [
+    (0, 2, 0),
+    (2, 0, 'Fail'),
+    (4, 2, 2.0),
+    (-4, 2, -2.0),
+    (0.4, 0.2, 2.0),
+    (-0.4, -0.2, 2.0),
+    (complex(1, 1), complex(1, 1), 1),
+    (complex(-1, -1), complex(-1, -1), 1),
+]
 
 
 class TestCalc():
-    def test_add1(self):
-        calc = Calc()
-        assert calc.add(1, 2) == 3
+    def setup_class(self):
+        self.calc = Calc()
+
+    @pytest.mark.parametrize("a,b,result", test_add_date)
+    def test_add(self, a, b, result):
+        assert self.calc.add(a, b) == result
+
+    @pytest.mark.parametrize("a,b,result", test_div_date)
+    def test_div(self, a, b, result):
+        assert self.calc.div(a, b) == result
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pytest.main()
-
-# import unittest
-# import sys
-#
-# sys.path.append("..")
-# from python.calc import Calc
-#
-#
-# class TestCalc(unittest.TestCase):
-#     def setUp(self) -> None:
-#         self.calc = Calc()
-#
-#     def test_add_1(self):
-#         self.assertEqual(3, self.calc.add(1, 2))
-#
-#     def test_add_2(self):
-#         self.assertEqual(300, self.calc.add(100, 200))
-#
-# if __name__=="__main__":
-#     unittest.main()
